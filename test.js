ad2c38584460cc7a6efb49f67210fd74f995b0ab
Jackson A Stone <jacksonstone1@Jacksons-MBP.wireless.utc.edu>
2016-04-08 18:06:10 -0400

    no more need to import anything I believe, and should do better on memory

--- test.js
+++ test.js
@@ -1,28 +1,25 @@
-var fsp = require('fs-promise');
 var fileName = process.argv[2];
-var DBText = [];
 var counts = {};
 
-fsp.readFile(fileName, 'utf8')
-.then((data, err)=>{
-  if(err)console.log(err);
-  else {
-    data = data.split("\n");
-    for(var i in data){
-      var choppedUpData = data[i].split(',');
-      if(choppedUpData[0] && choppedUpData[1]){
-        if(counts[choppedUpData[1]])
-        {
-          counts[choppedUpData[1]]++;
-        }
-        else {
-          counts[choppedUpData[1]] = 1;
-        }
-      }
-    }
-    for(var j in counts)
+var lineReader = require('readline').createInterface({
+  input: require('fs').createReadStream(fileName)
+});
+
+lineReader.on('line', function (data) {
+  data = data.split(',');
+  if(data[0] && data[1]){
+    if(counts[data[1]])
     {
-      console.log(j+','+counts[j]);
+      counts[data[1]]++;
     }
+    else {
+      counts[data[1]] = 1;
+    }
+  }
+});
+lineReader.on('close', function (line) {
+  for(var j in counts)
+  {
+    console.log(j+','+counts[j]);
   }
 });
